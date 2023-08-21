0e6fbbba810e2dbbb701712ef4c8d93c8565d8f3
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-08-22 00:53:11 +0300

    Presentation ve Web api katmanlarının controller bağlanması

--- CleanArchitecture.WebApi/Program.cs
+++ CleanArchitecture.WebApi/Program.cs
@@ -2,8 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers()
+    .AddApplicationPart(typeof(CleanArchitecture.Presentation.AssemblyReference).Assembly);
+
+
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
