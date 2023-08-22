462fae27751d3b57324b2b06cbaee7bc40f7c07b
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-08-23 00:53:00 +0300

    EntityTypeConfiguration, ModelCreating, SaveChangeAsync

--- CleanArchitecture.Domain/Abstractions/Entity.cs
+++ CleanArchitecture.Domain/Abstractions/Entity.cs
@@ -8,6 +8,10 @@ namespace CleanArchitecture.Domain.Abstractions
 {
     public abstract class Entity
     {
+        public Entity()
+        {
+            Id = Guid.NewGuid().ToString();
+        }
         public string Id { get; set; }
         public DateTime CreatedDate { get; set; }
         public DateTime? UpdatedDate { get; set; }
--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CleanArchitecture.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,23 @@ namespace CleanArchitecture.Persistance.Context
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
+        protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReferance).Assembly);
+        //burada ki assembly referansı sayesinde 100 tane de configuration da eklense otomatik olarak DbContext e bağlar.
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var entries = ChangeTracker.Entries<Entity>();
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property(p => p.CreatedDate).CurrentValue = DateTime.Now;
+                if (entry.State == EntityState.Modified)
+                    entry.Property(p => p.UpdatedDate).CurrentValue = DateTime.Now;
+            }
+            return base.SaveChangesAsync(cancellationToken);
+        }
+        //Burada ChangeTracker ile Entity içindeki propertyler alınır kaydetme işlemi öncesinde ve createdDate ve UpdatedDate değerleri burada doldurulur. 
+    
     }
 }
