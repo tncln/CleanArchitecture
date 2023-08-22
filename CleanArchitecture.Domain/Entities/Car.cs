61cd16f30592ead83ff13a1df0fe1663d70341b4
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-08-23 00:26:04 +0300

    car entity add

--- CleanArchitecture.Domain/Entities/Car.cs
+++ CleanArchitecture.Domain/Entities/Car.cs
@@ -1,6 +1,11 @@
-﻿namespace CleanArchitecture.Domain.Entities
+﻿using CleanArchitecture.Domain.Abstractions;
+
+namespace CleanArchitecture.Domain.Entities
 {
-    public sealed class Car
+    public sealed class Car : Entity
     {
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public int EnginePower { get; set; }
     }
 }
