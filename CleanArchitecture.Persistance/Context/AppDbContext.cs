5fc39ec25cf2556a4e9c2831851158ad7ef01f8f
Adem Tunçalın <31670075+tncln@users.noreply.github.com>
2023-09-10 01:56:12 +0300

    Identity Kurulum

--- CleanArchitecture.Persistance/Context/AppDbContext.cs
+++ CleanArchitecture.Persistance/Context/AppDbContext.cs
@@ -1,4 +1,7 @@
 ﻿using CleanArchitecture.Domain.Abstractions;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
@@ -8,14 +11,23 @@ using System.Threading.Tasks;
 
 namespace CleanArchitecture.Persistance.Context
 {
-    public sealed class AppDbContext : DbContext
+    public sealed class AppDbContext : IdentityDbContext<User, IdentityRole, string>
     {
         public AppDbContext(DbContextOptions options) : base(options)
         {
         }
-        protected override void OnModelCreating(ModelBuilder modelBuilder) =>
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.ApplyConfigurationsFromAssembly(typeof(AssemblyReferance).Assembly);
-        //burada ki assembly referansı sayesinde 100 tane de configuration da eklense otomatik olarak DbContext e bağlar.
+            //burada ki assembly referansı sayesinde 100 tane de configuration da eklense otomatik olarak DbContext e bağlar.
+
+            modelBuilder.Ignore<IdentityUserLogin<string>>();
+            modelBuilder.Ignore<IdentityUserRole<string>>();
+            modelBuilder.Ignore<IdentityUserClaim<string>>();
+            modelBuilder.Ignore<IdentityUserToken<string>>();
+            modelBuilder.Ignore<IdentityRoleClaim<string>>();
+            modelBuilder.Ignore<IdentityRole<string>>();
+        }
+
 
         public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
         {
